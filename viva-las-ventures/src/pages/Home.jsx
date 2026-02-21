5b42cdba27eb6c1d42e7f8c1a7f9d1aab50a9abc
Si_mpl <5trace.ed@gmail.com>
2026-02-21 07:47:15 -0800

    idc bru

--- viva-las-ventures/src/pages/Home.jsx
+++ viva-las-ventures/src/pages/Home.jsx
@@ -248,6 +248,10 @@ export default function Home() {
         </div>
       </main>
 
+
+      {/* ===== CALENDAR ==== */}
+      
+
       {/* ===== FOOTER ===== */}
       <footer className="border-t border-white/5 py-8 px-6">
         <div className="max-w-7xl mx-auto flex flex-col sm:flex-row items-center justify-between gap-4">
